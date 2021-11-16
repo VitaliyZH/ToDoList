c6dabc860c611d5ee10c894987e60c22737ff619
VitaliyZH <zhilyakov.vitaliy@yandex.ru>
2021-11-16 15:54:21 +0400

    azaz

--- ToDo.js
+++ ToDo.js
@@ -1,11 +1,42 @@
 "use strict"
+let button = document.querySelector(".add-btn");
+let input = document.querySelector(".input");
+let toDoText = "";
 
-function createToDo() {
-    let div = document.createElement("div");
-    div.innerHTML = "sgfsdg";
-    div.className = "alert";
-    document.body.append(div);
+//input.addEventListener("input", () => toDoText = input.value);
+
+
+function addToActive() {
+    let task = document.createElement("div");
+    let taskText = document.createElement("div");
+    let taskChecked = document.createElement("input");
+    let a = document.querySelector(".task__active")
+    let text = input.value;
+    taskText.innerHTML = input.value;
+    task.className = "task";
+    taskChecked.type = "checkbox";
+    a.append(task);
+    task.append(taskChecked);
+    task.append(taskText);
+    input.value = "";
+    taskChecked.addEventListener("change", () => {
+        let completedTask = document.createElement("div");
+        let a = document.querySelector(".task__completed")
+        completedTask.className = "task";
+        completedTask.innerHTML = text;
+        a.append(completedTask);
+        task.remove();
+    });
+}
+
+function moveToCompleted(text, el) {
+    let task = document.createElement("div");
+    let a = document.querySelector(".task__completed")
+    task.className = "task";
+    task.innerHTML = text;
+    a.append(task);
+    el.remove();
 
 }
-let button = document.querySelector(".add-btn");
-button.addEventListener("click", createToDo);
+
+button.addEventListener("click", addToActive);
